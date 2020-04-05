7200ca4b130b261776d877f67ffe04362658c019
KDANext <dima.1998.gig@mail.ru>
2020-04-06 00:58:09 +0400

    Lab VH1

---
+++ Blacksmith'sWorkshopBusinessLogic/BindingModels/StorageAddBilletBiningModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Blacksmith_sWorkshopBusinessLogic.BindingModels
+{
+    public class StorageAddBilletBindingModel
+    {
+        public int StorageId { set; get; }
+        public int BilletId { set; get; }
+        public int BilletCount { set; get; }
+    }
+}
---
+++ Blacksmith'sWorkshopBusinessLogic/BindingModels/StorageBindingModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Blacksmith_sWorkshopBusinessLogic.BindingModels
+{
+    public class StorageBindingModel
+    {
+        public int? Id { set; get; }
+        public string StorageName { set; get; }
+        public Dictionary<int, (string, int)> StoragedBillets { get; set; } //(name, count)
+    }
+}
