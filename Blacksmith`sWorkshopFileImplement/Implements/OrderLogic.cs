8940248e03f4e73a09a3970982fd6fb2cb72cd97
KDANext <dima.1998.gig@mail.ru>
2020-04-05 17:31:59 +0400

    fix nameSpace in file implement

--- Blacksmith`sWorkshopFileImplement/Implements/OrderLogic.cs
+++ Blacksmith`sWorkshopFileImplement/Implements/OrderLogic.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Blacksmith_sWorkshopDatebaseImplement
+namespace Blacksmith_sWorkshopFileImplement
 {
     public class OrderLogic : IOrderLogic
     {
--- Blacksmith`sWorkshopFileImplement/Implements/ProductLogic.cs
+++ Blacksmith`sWorkshopFileImplement/Implements/ProductLogic.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Blacksmith_sWorkshopDatebaseImplement.Implements
+namespace Blacksmith_sWorkshopFileImplement.Implements
 {
     public class ProductLogic : IProductLogic
     {
