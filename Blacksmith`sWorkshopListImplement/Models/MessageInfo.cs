73ab4ff3ad3ceada985e277da5902b395b12b356
KDANext <dima.1998.gig@mail.ru>
2020-06-10 22:28:44 +0400

    Update MessageInfo.cs

--- Blacksmith`sWorkshopListImplement/Models/MessageInfo.cs
+++ Blacksmith`sWorkshopListImplement/Models/MessageInfo.cs
@@ -12,6 +12,5 @@ namespace Blacksmith_sWorkshopListImplement.Models
         public DateTime DateDelivery { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
-        public virtual Client Client { get; set; }
     }
 }
